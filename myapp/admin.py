4b3cff3245b9c8b588e503310d2c781ea120ccf2
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2018-12-04 13:53:28 +0100

    Visualization in the admin page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Question,Choice
+
 class ChoiceinLine(admin.TabularInline):
     model = Choice
     extra = 0
+
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text','cat')
     fieldsets = [
             (None,{'fields': ['question_text','score']}),
             ('Categoria', {'fields': ['cat']})
