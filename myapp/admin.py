0de6464440322b80fb45c9c07ee8c78122cff488
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2018-12-04 16:33:55 +0100

    Possibility to show the result of the test

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Question,Choice
+from .models import Question,Choice,People
+
+class Peoples(admin.TabularInline):
+    model = People
+    extra = 0
+
 
 class ChoiceinLine(admin.TabularInline):
     model = Choice
     extra = 0
+
+class OnePeople(admin.ModelAdmin):
+    list_display = ('id_test','extraversion','agreeableness','neuroticism','coscientiousness','openness')
+
+
+admin.site.register(People,OnePeople)
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text','cat')
