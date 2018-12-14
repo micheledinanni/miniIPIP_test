e16d2dfa0d1d857cd06f34ddafaf8d7b87c88250
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2018-12-14 18:59:07 +0100

    Modified the admin interface

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,27 +1,48 @@
 from django.contrib import admin
-from .models import Question,Choice,People
+from django.http import HttpResponseRedirect
+from django.utils.crypto import get_random_string
+from .models import Question,Choice,Email,People,PeopleQuestions
+from django.core.mail import send_mail
 
-class Peoples(admin.TabularInline):
-    model = People
-    extra = 0
+class PeopleAvg(admin.ModelAdmin):
+    list_display = ('id_test','extraversion','agreeableness','coscientiousness','openness','neuroticism')
 
+admin.site.register(People,PeopleAvg)
+
+class PeopleQ(admin.ModelAdmin):
+    list_display = ('id_test','question','score','cat')
+
+admin.site.register(PeopleQuestions,PeopleQ)
 
 class ChoiceinLine(admin.TabularInline):
     model = Choice
     extra = 0
 
-class OnePeople(admin.ModelAdmin):
-    list_display = ('id_test','extraversion','agreeableness','neuroticism','coscientiousness','openness')
-
-
-admin.site.register(People,OnePeople)
-
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text','cat')
     fieldsets = [
-            (None,{'fields': ['question_text','score']}),
+            (None,{'fields': ['question_text']}),
             ('Categoria', {'fields': ['cat']})
                 ]
     inlines = [ChoiceinLine]
-
 admin.site.register(Question, QuestionAdmin)
+
+class Field_mail(admin.TabularInline):
+    model = Email
+
+class Send_email(admin.ModelAdmin):
+    #change_form_template = "admin/change_form.html"
+    def response_change(self, request, obj):
+        id = get_random_string(length=6)
+        if "send-mail" in request.POST:
+            email_to_send = request.POST.get('email')
+            print (request.POST.get('email'))
+            send_mail("Mini-ipip test",
+                          "Here's the mini ipip test: http://127.0.0.1:8000/myapp?id={0}".format(id),
+                          "michele.dinanni1@gmail.com",
+                          recipient_list=[email_to_send, ],
+                          fail_silently=False)
+            self.message_user(request,"The email has been sent!")
+        return HttpResponseRedirect(".")
+
+admin.site.register(Email,Send_email)
