f89ed200c065757dd11ee5e55af110bb0ff889d3
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2019-01-24 19:19:36 +0100

    Show into admin page 'FolderInfo'

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,40 +2,47 @@ from tkinter import filedialog
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.utils.crypto import get_random_string
-from .models import Question,Choice,Email,People,PeopleQuestions,EmailToken
+from .models import Question,Choice,Email,Result,Question_people,EmailToken,FurtherPeopleInfo
 from django.core.mail import send_mail
 from django.contrib.auth.models import Group,User
-from django.core import serializers
 import tkinter,json
-from django.forms.models import model_to_dict
+
 #action to save the results into file .json
 def writeToJSONFile(path,fileName,data):
     filePathNameWExt = '' + path + '/' + fileName + '.json'
     with open(filePathNameWExt,'w') as fp:
         json.dump(data,fp)
 
-def save_as_json_people_avg(modeladmin, request, queryset):
-    results = list(People.objects.values())
-    writeToJSONFile('./','peopleAvg',results)
+def save_as_json_results(modeladmin, request, queryset):
+    top = tkinter.Tk()
+    in_path = filedialog.askdirectory()
+    top.destroy()
+    top.mainloop()
+    results = list(Result.objects.values())
+    writeToJSONFile(in_path,'Results',results)
 
 def save_as_json_people_quest(modeladmin, request, queryset):
-    results = list(PeopleQuestions.objects.values())
-    writeToJSONFile('./', 'peopleQuestions', results)
+    top = tkinter.Tk()
+    in_path = filedialog.askdirectory()
+    top.destroy()
+    top.mainloop()
+    results = list(Question_people.objects.values())
+    writeToJSONFile(in_path, 'PeopleQuestions', results)
 
 class PeopleAvg(admin.ModelAdmin):
-    actions = [save_as_json_people_avg,]
-    list_display = ('id_test','extraversion','agreeableness','coscientiousness','openness','neuroticism')
+    actions = [save_as_json_results,]
+    list_display = ('id_test','email','openness','coscientiousness','extraversion','agreeableness','neuroticism')
 
 class EmailTokenOne(admin.ModelAdmin):
-    list_display = ('id_test','mail')
+    list_display = ('id_test','email')
 
-admin.site.register(People,PeopleAvg)
+admin.site.register(Result,PeopleAvg)
 
 class PeopleQ(admin.ModelAdmin):
     actions = [save_as_json_people_quest,]
-    list_display = ('id_test','question','score','cat')
+    list_display = ('id_test','email','question','score','cat')
 
-admin.site.register(PeopleQuestions,PeopleQ)
+admin.site.register(Question_people,PeopleQ)
 
 class ChoiceinLine(admin.TabularInline):
     model = Choice
@@ -57,8 +64,8 @@ class Send_email(admin.ModelAdmin):
     change_form_template = "admin/change_.html"
     def response_change(self, request, obj):
         if "send-mail" in request.POST:
-            #take the emails from text area anc send to people with different token
-            email_to_send = request.POST.get('email').split(',')
+            #take the emails from text area and send to people with different token
+            email_to_send = request.POST.get('email_comma_separated').split(',')
             for email in email_to_send:
                 id = get_random_string(length=6)
                 oggetto = request.POST.get('oggetto')
@@ -69,14 +76,18 @@ class Send_email(admin.ModelAdmin):
                       recipient_list=[email],
                       fail_silently=False)
                 #save the token and the email in EmailToken table
-                q = EmailToken(id_test=id,mail=email)
+                q = EmailToken(id_test=id,email=email)
                 q.save()
-            self.message_user(request, "The email has been sent!")
+            self.message_user(request, "The emails have been sent!")
             return HttpResponseRedirect(".")
         return super().response_change(request, obj)
 
 admin.site.register(Email,Send_email)
 admin.site.register(EmailToken,EmailTokenOne)
+
+class FurtherInformation(admin.ModelAdmin):
+    list_display = ('id_test','email','date_of_birth','born','gender','ethnicity','level_school','employment')
+admin.site.register(FurtherPeopleInfo,FurtherInformation)
 
 #remove Group and User from Admin page
 admin.site.unregister(User)
