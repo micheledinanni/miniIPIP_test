ee51c4d97ac90c6102893cefea79e46a9711636a
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2019-01-24 19:22:59 +0100

    Add table 'FurtherInfo'

--- myapp/models.py
+++ myapp/models.py
@@ -16,26 +16,40 @@ class Choice(models.Model):
         return self.choice_text
 
 class Email(models.Model):
-    email = models.TextField()
-    text = models.TextField()
-    oggetto = models.TextField()
+    email_comma_separated = models.TextField(default='Enter the emails here:')
+    text = models.TextField(default='Enter the text here:')
+    oggetto = models.TextField(default='Enter the object here:')
     def __str__(self):
-        return self.email
+        return self.email_comma_separated
 
-class People(models.Model):
-    id_test = models.CharField(max_length=10,primary_key=True,default=0)
-    extraversion = models.FloatField(default=0)
-    coscientiousness = models.FloatField(default=0)
-    agreeableness = models.FloatField(default=0)
-    openness = models.FloatField(default=0)
-    neuroticism = models.FloatField(default=0)
+class Result(models.Model):
+    id_test = models.CharField(max_length=10,primary_key=True)
+    email = models.CharField(max_length=50)
+    extraversion = models.FloatField(default=0.0)
+    coscientiousness = models.FloatField(default=0.0)
+    agreeableness = models.FloatField(default=0.0)
+    openness = models.FloatField(default=0.0)
+    neuroticism = models.FloatField(default=0.0)
 
-class PeopleQuestions(models.Model):
-    id_test = models.CharField(max_length=10,default=0)
+class Question_people(models.Model):
+    id_test = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
     question = models.IntegerField(default=0)
     score = models.IntegerField(default=0)
-    cat = models.CharField(default='None',max_length=5)
+    cat = models.CharField(max_length=3)
 
 class EmailToken(models.Model):
-    id_test = models.CharField(max_length=6)
-    mail = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    id_test = models.CharField(max_length=10)
+
+class FurtherPeopleInfo(models.Model):
+    id_test = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    born = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    ethnicity = models.CharField(max_length=100)
+    level_school = models.CharField(max_length=100)
+    employment = models.CharField(max_length=100)
+
+
