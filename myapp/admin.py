f84ef1439323a4d50bf82f5e03be3fb350f2a387
Michele Di Nanni <37437292+micheledinanni@users.noreply.github.com>
2019-01-25 11:23:27 +0100

    Add new function to verify the emails to send

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,7 +5,7 @@ from django.utils.crypto import get_random_string
 from .models import Question,Choice,Email,Result,Question_people,EmailToken,FurtherPeopleInfo
 from django.core.mail import send_mail
 from django.contrib.auth.models import Group,User
-import tkinter,json
+import tkinter,json,re
 
 #action to save the results into file .json
 def writeToJSONFile(path,fileName,data):
@@ -64,23 +64,31 @@ class Send_email(admin.ModelAdmin):
     change_form_template = "admin/change_.html"
     def response_change(self, request, obj):
         if "send-mail" in request.POST:
-            #take the emails from text area and send to people with different token
+            #take the emails from text area and send to people with different tokens
             email_to_send = request.POST.get('email_comma_separated').split(',')
-            for email in email_to_send:
+            check_send_email(email_to_send,request)
+            self.message_user(request,"Email checked and sent succesfully!")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
+
+def check_send_email(email_to_send,request):
+    for email in email_to_send:
+        # verify that the emails are correct syntactically
+        if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            # check that the email hasn't already been sent and there are not duplicates
+            if email not in EmailToken.objects.values_list('email', flat=True):
                 id = get_random_string(length=6)
                 oggetto = request.POST.get('oggetto')
                 text_to_send = request.POST.get('text')
                 send_mail(oggetto,
-                      text_to_send + id,
-                      "michele.dinanni1@gmail.com",
-                      recipient_list=[email],
-                      fail_silently=False)
-                #save the token and the email in EmailToken table
-                q = EmailToken(id_test=id,email=email)
+                          text_to_send + id,
+                          "michele.dinanni1@gmail.com",
+                          recipient_list=[email],
+                          fail_silently=False)
+                flag = True
+                # save the token and the email in EmailToken table
+                q = EmailToken(id_test=id, email=email)
                 q.save()
-            self.message_user(request, "The emails have been sent!")
-            return HttpResponseRedirect(".")
-        return super().response_change(request, obj)
 
 admin.site.register(Email,Send_email)
 admin.site.register(EmailToken,EmailTokenOne)
