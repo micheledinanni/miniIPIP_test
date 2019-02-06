2d3b0d1b95d7e27997513d32a05571465f4b7d89
Fabio Calefato <fabio.calefato@uniba.it>
2019-02-06 16:36:47 +0100

    Edit ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = secret()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
