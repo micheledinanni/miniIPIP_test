e3244f5f8cbd63934df1704432b5832f6e659425
micheledinanni <michele.dinanni@libero.it>
2019-02-12 12:51:58 +0100

    Format id when sending text email

--- miniipip/admin_sending_mail.py
+++ miniipip/admin_sending_mail.py
@@ -5,35 +5,41 @@ import yaml
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
 def email_from():
-    with open(os.path.join("myproject","cfg","config.yml"),"r") as ymlfile:
+    with open(os.path.join("myproject", "cfg", "config.yml"), "r") as ymlfile:
         cfg = yaml.load(ymlfile)
         email = cfg['email'].__getitem__('email_from')
     return email
 
+
 def email_host():
-    with open(os.path.join("myproject","cfg","config.yml"),"r") as ymlfile:
+    with open(os.path.join("myproject", "cfg", "config.yml"), "r") as ymlfile:
         cfg = yaml.load(ymlfile)
         host = cfg['email'].__getitem__('email_host')
     return host
 
+
 def email_host_user():
-    with open(os.path.join("myproject","cfg","config.yml"),"r") as ymlfile:
+    with open(os.path.join("myproject", "cfg", "config.yml"), "r") as ymlfile:
         cfg = yaml.load(ymlfile)
         host_user = cfg['email'].__getitem__('email_host_user')
     return host_user
 
+
 def email_host_passwd():
-    with open(os.path.join("myproject","cfg","config.yml"),"r") as ymlfile:
+    with open(os.path.join("myproject", "cfg", "config.yml"), "r") as ymlfile:
         cfg = yaml.load(ymlfile)
         host_passwd = cfg['email'].__getitem__('email_host_passwd')
     return host_passwd
 
+
 def email_port():
-    with open(os.path.join("myproject","cfg","config.yml"),"r") as ymlfile:
+    with open(os.path.join("myproject", "cfg", "config.yml"), "r") as ymlfile:
         cfg = yaml.load(ymlfile)
         host_port = cfg['email'].__getitem__('email_port')
     return host_port
+
 
 def main_send_email(subject, text_sending, email):
     from_address = email_from()
@@ -56,4 +62,5 @@ def main_send_email(subject, text_sending, email):
         time.sleep(9)
         server.quit()
         return 1
-    except Exception: return 0
+    except Exception:
+        return 0
