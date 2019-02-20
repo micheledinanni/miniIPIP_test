28d18d4a3016e03e6233210a72e304520fe77a13
micheledinanni <michele.dinanni@libero.it>
2019-02-20 17:18:24 +0100

    Use logging in ajax

--- myproject/ajax.py
+++ myproject/ajax.py
@@ -1,10 +1,14 @@
 import json
-
+from .logger import logger_file
 from django.http import JsonResponse
 
 
 def validate(request, id):
-    with open('myproject/ajax_files/status.json', 'r') as jsonfile:
-        data = json.load(jsonfile)
-        jsonfile.close()
-        return JsonResponse(data)
+    try:
+        with open('myproject/ajax_files/status.json', 'r') as jsonfile:
+            data = json.load(jsonfile)
+            jsonfile.close()
+            return JsonResponse(data)
+    except Exception as e:
+        error = str(e)
+        logger_file(error)
