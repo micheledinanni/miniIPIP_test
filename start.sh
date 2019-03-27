afd1e0c99a9ac022d570bf28d6c0925d7989c459
Fabio Calefato <fcalefato@gmail.com>
2019-03-27 14:53:37 +0100

    Update echo message

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+file=.first_run
+if [ ! -e "$file" ]; then
+    echo "Performin first time setup"
+    python manage.py makemigrations miniipip
+    python manage.py migrate
+    python manage.py loaddata db.json
+    touch "$file"
+    echo "Done"
+fi 
+
 # Start Gunicorn processes
-echo Starting Gunicorn.
+echo "Starting Gunicorn"
 
 exec gunicorn myproject.wsgi:application \
     --bind 0.0.0.0:8000 \
